5da0f800861f9b32ce1ef5d6ed30b11b9b6180f5
Aniket Sanjay Suryavanshi <99653627+AniketSuryavanshi3@users.noreply.github.com>
2025-02-14 23:42:17 +0530

    logic 2nd

---
+++ Pattern/StarPattern3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class StarPattern3 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i=0;i<n;i++)
+		{
+			for (int space=0;space<i;space++ )
+			{
+				System.out.print(" ");
+			}
+			for (int star=0;star<n-i;star++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
